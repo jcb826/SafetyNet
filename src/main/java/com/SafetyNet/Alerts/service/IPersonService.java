a858d045c05e4e4f0dd0bad48a493ef89a73b19d
jcb826 <72393579+jcb826@users.noreply.github.com>
2021-04-13 09:07:09 +0200

    Added interfaces

--- src/main/java/com/SafetyNet/Alerts/service/IPersonService.java
+++ src/main/java/com/SafetyNet/Alerts/service/IPersonService.java
@@ -5,54 +5,16 @@ import com.SafetyNet.Alerts.service.dto.ChildAlertDto;
 import com.SafetyNet.Alerts.service.dto.FireDto;
 import com.SafetyNet.Alerts.service.dto.PersonInfoDto;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 
 public interface IPersonService {
 
-
     List<String> findAllEmailsByCity(String city);
 
     List<ChildAlertDto> findAllchildsUnder18ByAddress(String address);
 
-
     List<PersonInfoDto> findAllpersonsWithMedicalRecords(String firstName, String lastName);
-
-
-    private int computeAge(String birthdateOfPerson) {
-        Date date = null;
-        Calendar now = Calendar.getInstance();
-        Calendar birthDate = Calendar.getInstance();
-        try {
-            date = new SimpleDateFormat("dd/MM/yyyy").parse(birthdateOfPerson);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        birthDate.setTime(date);
-        if (birthDate.after(now)) {
-            throw new IllegalArgumentException("Can't be born in the future");
-        }
-        int year1 = now.get(Calendar.YEAR);
-        int year2 = birthDate.get(Calendar.YEAR);
-        int age = year1 - year2;
-        int month1 = now.get(Calendar.MONTH);
-        int month2 = birthDate.get(Calendar.MONTH);
-        if (month2 > month1) {
-            age--;
-        } else if (month1 == month2) {
-            int day1 = now.get(Calendar.DAY_OF_MONTH);
-            int day2 = birthDate.get(Calendar.DAY_OF_MONTH);
-            if (day2 > day1) {
-                age--;
-            }
-        }
-        return age;
-    }
-
 
     List<FireDto> findAllpersonsWithMedicalRecords(String address);
 
